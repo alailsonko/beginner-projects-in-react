0de689abc484efb8f26f1d9d181bad4a9f46658c
alailson <aalailson3@gmail.com>
2022-09-02 05:35:35 -0300

    routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from 'react-router-dom'
+
+const RoutesApp = () => {
+    return (
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>hello</div>} />
+        </Routes>
+      </BrowserRouter>
+    )
+}
+
+export default RoutesApp;
