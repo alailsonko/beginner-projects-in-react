51533d7ba46e09900ec900306be64797c165b51d
alailson <aalailson3@gmail.com>
2022-09-02 05:52:23 -0300

    formatted

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,14 @@
-import React from "react";
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const RoutesApp = () => {
-    return (
-        <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<div>hello</div>} />
-        </Routes>
-      </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>hello</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default RoutesApp;
